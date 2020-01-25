c2d77316f6e32977ee916a9117b76f0c957cd1e4
abdullokh <abdumannobov96@mail.ru>
2020-01-25 13:36:33 +0500

    js3

---
+++ script.js
@@ -0,0 +1,83 @@
+var time = +prompt('Введите время');
+switch (time) {
+    case 0:
+        alert('Полночь')
+        break;
+    case 1:
+        alert('Час ночи')
+        break;
+    case 2:
+        alert('Два часа ночи')
+        break;
+    case 3:
+        alert('Три часа ночи')
+        break;
+    case 4:
+        alert('Четыре часа ночи')
+        break;
+    case 5:
+        alert('Пять часов утра')
+        break;
+    case 6:
+        alert('Шесть часов утра')
+        break;
+    case 7:
+        alert('Семь часов утра')
+        break;
+    case 8:
+        alert('Восемь часов утра')
+        break;
+    case 9:
+        alert('Девять часов утра')
+        break;
+    case 10:
+        alert('Десять часов утра')
+        break;
+    case 11:
+        alert('Одинадцать часов утра')
+        break;
+    case 12:
+        alert('Полдень')
+        break;
+        case 13:
+        alert('Час дня')
+        break;
+    case 14:
+        alert('Два часа дня')
+        break;
+    case 15:
+        alert('Три часа дня')
+        break;
+    case 16:
+        alert('Четыре часов дня')
+        break;
+    case 17:
+        alert('Пять часов дня')
+        break;
+    case 18:
+        alert('Шесть часов вечера')
+        break;
+    case 19:
+        alert('Семь часов вечера')
+        break;
+    case 20:
+        alert('Восемь часов вечера')
+        break;
+    case 21:
+        alert('Девять часов вечера')
+        break;
+    case 22:
+        alert('Десять часов вечара')
+        break;
+    case 23:
+        alert('Одинадцать часов вечера')
+        break;
+    case 24:
+        alert('Полночь')
+        break;
+
+
+    default:
+        alert('Ошибка')
+        break;
+}
